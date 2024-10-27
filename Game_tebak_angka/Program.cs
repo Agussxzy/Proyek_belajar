e886ba1720e8e54e4ba081a474b72b98d24265d0
Agussxzy <muhammadagus8766@gmail.com>
2024-10-27 21:12:43 +0700

    first commit

---
+++ Game_tebak_angka/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Game_tebak_angka;
+
+public static class Program
+{
+	public static void Main()
+	{
+		    Random random = new Random();
+		    Double angkaacak = random.Next(1,100);
+		
+				
+			Console.WriteLine("----------------");
+			Console.WriteLine("game tebak angka");
+			Console.WriteLine("----------------");
+			
+			while(true)
+			{
+				Console.WriteLine("masukan tebakan anda");
+				Double pilihanusr = Convert.ToDouble(Console.ReadLine());
+				if (pilihanusr == angkaacak)	
+				{
+					    Console.WriteLine("selamat kamu menang");
+						Console.WriteLine("kelik enter untuk lanjut");
+						Console.ReadKey();
+						Console.Clear();
+				}
+				else if(pilihanusr <angkaacak)	
+				{
+					    Console.WriteLine("angka terlalu rendah");
+						
+				}
+				else if(pilihanusr > angkaacak)		
+				{
+				Console.WriteLine("angka terlalu tinggi");
+				
+				}
+			}	
+
+	}
+}
